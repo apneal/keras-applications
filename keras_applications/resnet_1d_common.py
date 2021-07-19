d8da02b7e95c193f9bb9ae450bb2cb7d9a499bf6
guiCat <guillaume.cathelain@gmail.com>
2021-07-19 17:07:08 +0200

    Stacks 2,3,4,5 correction of pooling for ResNets

--- keras_applications/resnet_1d_common.py
+++ keras_applications/resnet_1d_common.py
@@ -106,6 +106,7 @@ def stack0(x, filters, blocks, stride1=2, name=None):
         x = block0(x, filters, conv_shortcut=False, name=name + '_block' + str(i))
     return x
 
+
 def block1(x, filters, kernel_size=3, stride=1,
            conv_shortcut=True, name=None):
     """A residual block.
@@ -122,33 +123,28 @@ def block1(x, filters, kernel_size=3, stride=1,
     # Returns
         Output tensor for the residual block.
     """
-    bn_axis = -1 if backend.image_data_format() == 'channels_last' else 1
+    bn_axis = 2 if backend.image_data_format() == 'channels_last' else 1
 
     if conv_shortcut is True:
-        #print('x', x.shape)
         shortcut = layers.Conv1D(4 * filters, 1, strides=stride,
                                  name=name + '_0_conv')(x)
-        #print('shortcut', shortcut.shape)
         shortcut = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
                                              name=name + '_0_bn')(shortcut)
     else:
         shortcut = x
 
     x = layers.Conv1D(filters, 1, strides=stride, name=name + '_1_conv')(x)
-    #print('x1', x.shape)
     x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
                                   name=name + '_1_bn')(x)
     x = layers.Activation('relu', name=name + '_1_relu')(x)
 
     x = layers.Conv1D(filters, kernel_size, padding='SAME',
                       name=name + '_2_conv')(x)
-    #print('x2', x.shape)
     x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
                                   name=name + '_2_bn')(x)
     x = layers.Activation('relu', name=name + '_2_relu')(x)
 
     x = layers.Conv1D(4 * filters, 1, name=name + '_3_conv')(x)
-    #print('x3', x.shape)
     x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
                                   name=name + '_3_bn')(x)
 
@@ -192,7 +188,7 @@ def block2(x, filters, kernel_size=3, stride=1,
     # Returns
         Output tensor for the residual block.
     """
-    bn_axis = -1 if backend.image_data_format() == 'channels_last' else 1
+    bn_axis = 2 if backend.image_data_format() == 'channels_last' else 1
 
     preact = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
                                        name=name + '_preact_bn')(x)
@@ -239,86 +235,6 @@ def stack2(x, filters, blocks, stride1=2, name=None):
     for i in range(2, blocks):
         x = block2(x, filters, name=name + '_block' + str(i))
     x = block2(x, filters, stride=stride1, name=name + '_block' + str(blocks))
-    return x
-
-
-def block3(x, filters, kernel_size=3, stride=1, groups=32,
-           conv_shortcut=True, name=None):
-    """A residual block.
-
-    # Arguments
-        x: input tensor.
-        filters: integer, filters of the bottleneck layer.
-        kernel_size: default 3, kernel size of the bottleneck layer.
-        stride: default 1, stride of the first layer.
-        groups: default 32, group size for grouped convolution.
-        conv_shortcut: default True, use convolution shortcut if True,
-            otherwise identity shortcut.
-        name: string, block label.
-
-    # Returns
-        Output tensor for the residual block.
-    """
-    bn_axis = -1 if backend.image_data_format() == 'channels_last' else 1
-
-    if conv_shortcut is True:
-        shortcut = layers.Conv1D((64 // groups) * filters, 1, strides=stride,
-                                 use_bias=False, name=name + '_0_conv')(x)
-        shortcut = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
-                                             name=name + '1D_0_bn')(shortcut)
-    else:
-        shortcut = x
-
-    x = layers.Conv1D(filters, 1, use_bias=False, name=name + '_1_conv')(x)
-    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
-                                  name=name + '_1_bn')(x)
-    x = layers.Activation('relu', name=name + '_1_relu')(x)
-
-    c = filters // groups
-    x = layers.ZeroPadding1D(padding=0, name=name + '_2_pad')(x)
-    x = layers.DepthwiseConv1D(kernel_size, strides=stride, depth_multiplier=c,
-                               use_bias=False, name=name + '_2_conv')(x)
-    kernel = np.zeros((1, 1, filters * c, filters), dtype=np.float32)
-    for i in range(filters):
-        start = (i // c) * c * c + i % c
-        end = start + c * c
-        kernel[:, :, start:end:c, i] = 1.
-    x = layers.Conv1D(filters, 1, use_bias=False, trainable=False,
-                      kernel_initializer={'class_name': 'Constant',
-                                          'config': {'value': kernel}},
-                      name=name + '_2_gconv')(x)
-    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
-                                  name=name + '_2_bn')(x)
-    x = layers.Activation('relu', name=name + '_2_relu')(x)
-
-    x = layers.Conv1D((64 // groups) * filters, 1,
-                      use_bias=False, name=name + '_3_conv')(x)
-    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
-                                  name=name + '_3_bn')(x)
-
-    x = layers.Add(name=name + '_add')([shortcut, x])
-    x = layers.Activation('relu', name=name + '_out')(x)
-    return x
-
-
-def stack3(x, filters, blocks, stride1=2, groups=32, name=None):
-    """A set of stacked residual blocks.
-
-    # Arguments
-        x: input tensor.
-        filters: integer, filters of the bottleneck layer in a block.
-        blocks: integer, blocks in the stacked blocks.
-        stride1: default 2, stride of the first layer in the first block.
-        groups: default 32, group size for grouped convolution.
-        name: string, stack label.
-
-    # Returns
-        Output tensor for the stacked blocks.
-    """
-    x = block3(x, filters, stride=stride1, groups=groups, name=name + '_block1')
-    for i in range(2, blocks + 1):
-        x = block3(x, filters, groups=groups, conv_shortcut=False,
-                   name=name + '_block' + str(i))
     return x
 
 
@@ -588,51 +504,9 @@ def ResNet152V2(include_top=True,
                   **kwargs)
 
 
-def ResNeXt50(include_top=True,
-              weights=None,
-              input_tensor=None,
-              input_shape=None,
-              pooling=None,
-              classes=1000,
-              **kwargs):
-    def stack_fn(x):
-        x = stack3(x, 128, 3, stride1=1, name='conv2')
-        x = stack3(x, 256, 4, name='conv3')
-        x = stack3(x, 512, 6, name='conv4')
-        x = stack3(x, 1024, 3, name='conv5')
-        return x
-    return ResNet(stack_fn, False, False, 'resnext50',
-                  include_top, weights,
-                  input_tensor, input_shape,
-                  pooling, classes,
-                  **kwargs)
-
-
-def ResNeXt101(include_top=True,
-               weights=None,
-               input_tensor=None,
-               input_shape=None,
-               pooling=None,
-               classes=1000,
-               **kwargs):
-    def stack_fn(x):
-        x = stack3(x, 128, 3, stride1=1, name='conv2')
-        x = stack3(x, 256, 4, name='conv3')
-        x = stack3(x, 512, 23, name='conv4')
-        x = stack3(x, 1024, 3, name='conv5')
-        return x
-    return ResNet(stack_fn, False, False, 'resnext101',
-                  include_top, weights,
-                  input_tensor, input_shape,
-                  pooling, classes,
-                  **kwargs)
-
-
 setattr(ResNet50, '__doc__', ResNet.__doc__)
 setattr(ResNet101, '__doc__', ResNet.__doc__)
 setattr(ResNet152, '__doc__', ResNet.__doc__)
 setattr(ResNet50V2, '__doc__', ResNet.__doc__)
 setattr(ResNet101V2, '__doc__', ResNet.__doc__)
 setattr(ResNet152V2, '__doc__', ResNet.__doc__)
-setattr(ResNeXt50, '__doc__', ResNet.__doc__)
-setattr(ResNeXt101, '__doc__', ResNet.__doc__)
